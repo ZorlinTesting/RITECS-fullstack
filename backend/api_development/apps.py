a589ba302bcb7d12168739c19dd75ba650eed4e6
Cedrick Valencia <zorlintesting@gmail.com>
2024-05-31 18:06:53 +0800

    Removed unnecessary files

--- backend/api_development/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class ApiDevelopmentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_development'
--- backend/api_development/utilities.py
+++
@@ -1,7 +0,0 @@
-import unicodedata
-
-def clean_string(input_string):
-   # Normalize string to decomposed form, then filter out non-ASCII and control characters
-    normalized_string = unicodedata.normalize('NFKD', input_string)
-    cleaned_string = ''.join(c for c in normalized_string if unicodedata.category(c) != 'Cc' and ord(c) < 128)
-    return cleaned_string
